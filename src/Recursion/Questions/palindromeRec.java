5b27baee161ec5f9ea6156bc1dddfba600d0ea82
harshau007 <amanupadhyay2004@gmail.com>
2022-12-14 12:25:15 +0530

    New recurssion questions added

---
+++ src/Recursion/Questions/palindromeRec.java
@@ -0,0 +1,7 @@
+package Recursion.Questions;
+
+public class palindromeRec {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Recursion/Questions/reverse.java
@@ -0,0 +1,32 @@
+package Recursion.Questions;
+
+public class reverse {
+    static int sum;
+    public static void main(String[] args) {
+        rev1(1234);
+        System.out.println(rev2(1234));
+    }
+
+
+    public static void rev1(int n) {
+        if (n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        sum = (sum * 10) + rem;
+        rev1(n/10);
+    }
+
+    public static int rev2(int n) {
+        int digits = (int) (Math.log10(n) + 1);
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits) {
+        if (n % 10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int) (Math.pow(10, digits -1)) + helper(n / 10, digits - 1);
+    }
+}
