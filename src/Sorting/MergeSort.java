6e156784bd0d309f1d1f59cbb660db15fe76256e
harshau007 <amanupadhyay2004@gmail.com>
2023-04-08 11:50:57 +0530

    added Merge Sort

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,62 @@
+package com.example.Sorting;
+
+import java.util.*;
+
+/* At every level, N elements are being merged
+*  Therefore k = log-base2(N)
+*  Time Complexity is O(N*log(N))
+*/
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 8, 3, 6, 2};
+        arr = mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
+}
